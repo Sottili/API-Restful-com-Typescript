887426c2d37a72cc7d0353a1ded56227b3d11dab
Filipe Sottili <91507688+Sottili@users.noreply.github.com>
2023-08-01 23:17:54 -0300

    Criação das validações

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "./controllers/movieControllers";
 
+//Validations
+import { validate } from "./middleware/handleValidation";
+
 const router = Router();
 
 export default router
   .get("/teste", (req: Request, res: Response) => {
     res.status(200).send("Api Working!");
   })
-  .post("/movie", createMovie);
+  .post("/movie", validate, createMovie);
