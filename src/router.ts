afcd12499da2adfbd771f162d36607960f6ddce5
Filipe Sottili <91507688+Sottili@users.noreply.github.com>
2023-08-01 23:59:49 -0300

    Criação do update e do delete.

--- src/router.ts
+++ src/router.ts
@@ -3,10 +3,13 @@ import {
   createMovie,
   findMovieById,
   getAllMovies,
+  removeMovie,
+  updateMovie,
 } from "./controllers/movieControllers";
 
 //Validations
 import { validate } from "./middleware/handleValidation";
+import { movieCreateValidation } from "./middleware/movieValidation";
 
 const router = Router();
 
@@ -14,6 +17,8 @@ export default router
   .get("/teste", (req: Request, res: Response) => {
     res.status(200).send("Api Working!");
   })
-  .post("/movie", validate, createMovie)
-  .get("movie/:id", findMovieById)
-  .get("movie", getAllMovies);
+  .post("/movie", movieCreateValidation(), validate, createMovie)
+  .get("/movie/:id", findMovieById)
+  .get("/movie", getAllMovies)
+  .delete("/movie/:id", removeMovie)
+  .patch("/movie/:id", movieCreateValidation(), validate, updateMovie);
